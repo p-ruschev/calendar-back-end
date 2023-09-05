88a8e9b3e6f4da29c886b90bca73a4d9a453d3cd
Rusi Ruschev <ruschevr@gmail.com>
2023-09-05 16:29:59 +0300

    clear calendarController from unnecessery commented lines

--- src/controllers/calendarController.js
+++ src/controllers/calendarController.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 
 const calendarService = require("../services/calendarService");
-//const { isGuest, isOwner, isNotOwner, hasNotJoined } = require('../middlewares/securityMiddleware');
 const { isAuth } = require("../middlewares/authMiddleware");
-//const { handleMultipleErrors } = require('../middlewares/errorHandler');
-//
+
 const renderAgenda = async (req, res) => {
   try {
     const year = req.query.year;
@@ -45,9 +43,6 @@ const searchNotes = async (req, res) => {
     }
     notesOption.owner = user;
 
-    //    const holidaysOption = {title: {$regex: title, $options: 'i'}, month, day};
-    //    const notesOption = {title: {$regex: title, $options: 'i'}, year, month, day, owner: user};
-
     const result = await calendarService.searchNotes(
       holidaysOption,
       notesOption
@@ -80,9 +75,7 @@ const initGetOneCalNote = async (req, res) => {
     console.log(error);
   }
 };
-//const renderCreate = (req, res) => {
-//  res.render('trips/trip-create');
-//};
+
 const initializeCreateHoliday = async (req, res) => {
   try {
     const data = {
@@ -98,7 +91,6 @@ const initializeCreateHoliday = async (req, res) => {
     res.status(200).json(holiday);
   } catch (error) {
     console.log("error");
-    //  handleMultipleErrors(res, error, 'trips/trip-create');
   }
 };
 
@@ -111,7 +103,6 @@ const initializeCreateNote = async (req, res) => {
     res.status(200).json(note);
   } catch (error) {
     console.log("error");
-    //  handleMultipleErrors(res, error, 'trips/trip-create');
   }
 };
 
@@ -123,7 +114,6 @@ const initializeEditNote = async (req, res) => {
     res.status(200).json({ note });
   } catch (error) {
     console.log("error");
-    //  handleMultipleErrors(res, error, 'trips/trip-create');
   }
 };
 
@@ -135,20 +125,8 @@ const initializeEditCalNote = async (req, res) => {
     res.status(200).json({ note });
   } catch (error) {
     console.log("error");
-    //  handleMultipleErrors(res, error, 'trips/trip-create');
   }
 };
-
-/*
-const initializeGetAll = async (req, res) => {
-  try {
-    const allPosters = await posterService.getAll();
-    res.status(200).json(allPosters);
-  } catch(error) {
-    console.log(error);
-  }
-}
-*/
 
 const initializeDeleteNote = async (req, res) => {
   try {
@@ -170,99 +148,16 @@ const initializeDeleteCalNote = async (req, res) => {
     console.log(error);
   }
 };
-//
-//
-//
-//const renderDetails = async (req, res, next) => {
-//  try {
-//    const tripId = req.params.tripId;
-//    const userId = req.user?._id;
-//    const trip = await tripService.getOne(tripId);
-//    const isCreator = userId == trip.creator._id;
-//    const hasJoined = trip.buddies.some(x => x._id == userId);
-//    const availableSeats = trip.seats == 0 ? false : true;
-//    const buddies =  trip.buddies.length > 0
-//      ? trip.buddies.map(x => x.email).join(', ')
-//      : null;
-//    res.render('trips/trip-details', {...trip, buddies, isCreator, hasJoined, availableSeats});
-//  } catch(error) {
-//    next(error);
-//  }
-//};
-//
-//const initializeDelete = async (req, res, next) => {
-//  try {
-//    await tripService.deleteOne(req.params.tripId);
-//    res.redirect('/trips');
-//  } catch(error) {
-//    next(error);
-//  }
-//};
-//
-//const initializeJoin = async (req, res, next) => {
-//  try {
-//    const tripId = req.params.tripId;
-//    const userId = req.user?._id;
-//    await tripService.join(tripId, userId);
-//    const trip = await tripService.getOne(tripId);
-//    const isCreator = userId == trip.creator._id;
-//    const hasJoined = trip.buddies.some(x => x._id == userId);
-//    const availableSeats = trip.seats == 0 ? false : true;
-//    const buddies =  trip.buddies.length > 0
-//      ? trip.buddies.map(x => x.email).join(', ')
-//      : null;
-//      res.redirect('/trips/' + tripId + '/details')
-//    //res.render('trips/trip-details', {...trip, buddies, isCreator, hasJoined, availableSeats});
-//  } catch(error) {
-//    next(error);
-//  }
-//};
-//
-//const renderEdit = async (req, res) => {
-//  res.render('trips/trip-edit', req.trip);
-//};
-//const initializeEdit = async (req, res) => {
-//   try {
-//    await tripService.editOne(req.params.tripId, req.body);
-//    res.redirect('/trips/' + req.params.tripId + '/details');
-//  } catch(error) {
-//    const _id = req.trip._id;
-//    handleMultipleErrors(res, error, 'trips/trip-edit', {...req.body, _id});
-//}
-//}
-////const renderSearch = async (req, res) => {
-////  res.render('search');
-////};
-////
-////const initializeSearch = async (req, res, next) => {
-//// try {
-////   const { type } = req.body;
-////   const offers = await search(type);
-////   const searched = true;
-////   res.render('search', {offers, searched});
-////
-//// } catch(error) {
-////   console.log(error);
-////    next(error);
-//// }
-////};
-////
+
 router.get("/", renderAgenda);
 router.get("/search", searchNotes);
-router.get("/edit-custom-note/:noteId", isAuth, initGetOneNote);
-router.get("/edit-calendar-note/:noteId", isAuth, initGetOneCalNote);
-//router.get('/create', isAuth, renderCreate);
-router.post("/create-holiday", isAuth, initializeCreateHoliday);
-router.post("/create-note", isAuth, initializeCreateNote);
-//router.get('/all', initializeGetAll);
-//router.get('/:tripId/details', renderDetails);
-router.delete("/delete-custom-note/:noteId", isAuth, initializeDeleteNote);
+router.get("/edit-custom-note/:noteId", initGetOneNote);
+router.get("/edit-calendar-note/:noteId", initGetOneCalNote);
+router.post("/create-holiday", initializeCreateHoliday);
+router.post("/create-note", initializeCreateNote);
+router.delete("/delete-custom-note/:noteId", initializeDeleteNote);
 router.delete("/delete-calendar-note/:noteId", isAuth, initializeDeleteCalNote);
-//router.get('/:tripId/join', isAuth, isNotOwner, hasNotJoined, initializeJoin);
-//router.get('/:tripId/edit', isAuth, isOwner, renderEdit);
 router.put("/edit-custom-note/:noteId", isAuth, initializeEditNote);
 router.put("/edit-calendar-note/:noteId", isAuth, initializeEditCalNote);
-//router.get('/search',  renderSearch);
-//router.post('/search',  initializeSearch);
-//
+
 module.exports = router;
